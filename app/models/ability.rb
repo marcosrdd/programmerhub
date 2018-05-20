28abdf5b0f3dbb2b4a3edcdaac1e1e1bfbcd3f1d
Unknown <marcosrdd@gmail.com>
2018-05-20 05:43:47 -0300

    Setup Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user
+      can :read, :all
+      can :manage, Post, user_id: user.id
+      can :manage, Follow, user_id: user.id
+      can :manage, Like, user_id: user.id
+      can :manage, Comment, user_id: user.id
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
