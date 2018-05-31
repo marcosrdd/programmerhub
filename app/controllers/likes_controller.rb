a4cd1cf2b00307ee6ed607a3257ae7750c6db34a
Unknown <marcosrdd@gmail.com>
2018-05-31 11:02:05 -0300

    Redirect not found page and created show.html post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,26 +2,25 @@ class LikesController < ApplicationController
   before_action :set_post
   before_action :set_event, only: [:destroy]
   load_and_authorize_resource
-  
+
   def create
     @like = Like.new(user: current_user, post: @post)
     respond_to do |format|
       if @like.save
         format.json { render json: @like }
-        format.html { redirect_to :back, notice: 'Post liked with success.' }
+        format.html { redirect_back fallback_location: user_url(current_user.id), notice: 'Post liked with success.' }
       else
         format.json { render json: @like.errors, status: :unprocessable_entity }
-        format.html { redirect_to :back }
+        format.html { redirect_back fallback_location: user_url(current_user.id) }
       end
     end
   end
 
   def destroy
     @like.destroy
-
     respond_to do |format|
       format.json { head :no_content }
-      format.html { redirect_to :back, notice: 'Post unliked with success.' }
+      format.html { redirect_back fallback_location: user_url(current_user.id), notice: 'Post unliked with success.' }
     end
   end
 
